e84914ddcb6230dc1ce6a825ef787c87563746b6
febc.yamamoto <yamamoto.febc@gmail.com>
2016-12-01 14:00:38 +0900

    change a routing func signature

--- routing/router_func.go
+++ routing/router_func.go
@@ -5,7 +5,7 @@ import (
 	"github.com/yamamoto-febc/jobq"
 )
 
-func Parallel(routes []string) jobq.JobRouterFunc {
+func Parallel(routes ...string) jobq.JobRouterFunc {
 	return func(queue *jobq.Queue, option *jobq.Option, req jobq.JobRequestAPI) {
 		for _, route := range routes {
 			queue.PushRequest(route, req.GetPayload())
